27862588c43ab5220dfbd58eecd2f6222b1cf4bf
Luiz Parente <lp.parente@gmail.com>
2022-01-17 20:29:02 -0500

    Classes.

---
+++ Classes/Models/Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes.Models {
+	public class Person {
+		// Properties
+		public string Name { get; set; }
+		public double Height { get; set; }
+		public DateTime DateOfBirth { get; set; }
+
+		// Methods
+
+		// Constructors
+
+		// Default constructor: Provided by C# by default when a
+		// constructor is not explicitly defined.
+
+		// If a constructor is provided, then
+		// C# will not provide a default constructor.
+
+		// We can use constructors to enforce business rules.
+		// For example, if business rules determine that
+		// Person objects MUST have a value initialized in DateOfBirth,
+		// Then, I can define a parameterized constructor that takes in
+		// a DateTime object, and eliminate the parameterless constructor.
+
+		// Ex.
+		// public Person(DateTime dob) { }
+
+		public Person() {
+			// This is NOT a default constructor.
+			// This is a PARAMETERLESS constructor.			
+		}
+
+		// Constructors are overridable.
+		public Person(DateTime dob) {
+			this.DateOfBirth = dob;
+		}
+	}
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Classes.Models;
+
+namespace Classes {
+	class Program {
+		static void Main(string[] args) {
+			Person john = new Person();
+			john.Name = "John";
+			john.Height = 185; //cm
+			john.DateOfBirth = new DateTime(2000, 01, 30);
+
+			Person joanne = new Person(new DateTime(1990, 2, 28));
+
+		}
+	}
+}
