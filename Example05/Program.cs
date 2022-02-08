a2f0f8423dc75413e76440f200b02f6a86341993
Luiz Parente <lp.parente@gmail.com>
2022-02-07 20:30:54 -0500

    Static classes.

---
+++ Example05/Program.cs
@@ -0,0 +1,19 @@
+﻿using Example05.Utilities;
+using System;
+
+namespace Example05 {
+	class Program {
+		static void Main(string[] args) {
+			// STATIC CLASSES
+			// Static classes cannot be instantiated.
+			Console.WriteLine("Hello World!");
+
+			//Console console = new Console();
+
+
+			// ----------
+
+			Database.Create("Whatever");
+		}
+	}
+}
---
+++ Example05/Utilities/Database.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Example05.Utilities {
+	public static class Database {
+		// A class that encapsulates database functionality.
+		// I.e. CRUD operations
+		// Static classes can only have static methods.
+
+		// FIELDS
+		private static readonly string connectionString = "Some connection string";
+
+		// PROPERTIES
+		public static string Constant { 
+			get {
+				return "VALUE";
+			}
+		}
+
+		// CONSTRUCTORS
+		static Database() { 
+			// Static constructor.
+			// Static initialization logic.
+		}
+
+		// METHODS
+		public static void Create(object entry) {
+			// Fancy database logic goes here.
+			Console.WriteLine("Entry inserted into database!");
+		}
+
+		public static object Get(string id) {
+			// Fancy database logic goes here.
+			return null;
+		}
+
+		public static void Update(object entry) {
+			// Fancy database logic goes here.
+			Console.WriteLine("Entry updated!");
+		}
+
+		public static void Delete(string id) {
+			// Fancy database logic goes here.
+			Console.WriteLine("Entry deleted!");
+		}
+	}
+}
