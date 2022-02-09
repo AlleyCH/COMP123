9d1229a4baef52b622fbedd6b57d5401cfdea944
Luiz Parente <lp.parente@gmail.com>
2022-02-08 21:51:37 -0500

    ToString method.

---
+++ Example06/Models/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Example06.Models {
+	public class Person {
+		public string Name { get; set; } 
+		public double Height { get; set; }
+		public DateTime DateOfBirth { get; set; }
+		public int Age { 
+			get {
+				int age = DateTime.Now.Year - this.DateOfBirth.Year;
+
+				if (DateTime.Now.DayOfYear < this.DateOfBirth.DayOfYear) {
+					age--;
+				}
+
+				return age;
+			}
+		}
+
+		// Methods
+		public override string ToString() {
+			return $"Name: {this.Name} \n" +
+				   $"Date Of Birth: {this.DateOfBirth:d} \n" +
+				   $"Age: {this.Age}";
+		}
+	}
+}
---
+++ Example06/Program.cs
@@ -0,0 +1,14 @@
+﻿using Example06.Models;
+using System;
+
+namespace Example06 {
+	class Program {
+		static void Main(string[] args) {
+			Person person = new Person();
+			person.Name = "John";
+			person.DateOfBirth = new DateTime(1985, 07, 15);
+
+			Console.WriteLine(person.ToString());
+		}
+	}
+}
