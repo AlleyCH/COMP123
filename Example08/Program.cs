72d07518fd13b64516dc895a3ad2b4bbe0c699e2
Luiz Parente <lp.parente@gmail.com>
2022-02-14 20:22:15 -0500

    File IO.

---
+++ Example08/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace Example08 {
+	class Program {
+		static void Main(string[] args) {
+			// FILE IO
+			// Writing and reading files to and from your computer's file system.
+
+			// Writing a string to a text file:
+			string content = "C# is awesome!";
+
+			if (Directory.Exists("C:\\temp")) {
+				File.WriteAllText("C:\\temp\\myFile.txt", content);
+			}
+			else {
+				throw new Exception("ERROR! Tried to create a file in a directory that does not exist.");
+			}
+
+			Console.WriteLine("File written successfully! \n");
+
+			// =======================================
+
+			// Loading contents from a text file:
+			string data = File.ReadAllText("C:\\temp\\myFile.txt");
+
+			Console.WriteLine("File contents successfully loaded!");
+			Console.WriteLine("Content is: ");
+			Console.WriteLine(data);
+		}
+	}
+}
