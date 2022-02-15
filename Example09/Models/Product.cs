052f3e01a935046e74a4cd64fb166a4b3c4e7814
Luiz Parente <lp.parente@gmail.com>
2022-02-14 20:22:21 -0500

    Serialization.

---
+++ Example09/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example09.Models {
+	public class Product {
+		public string ProductID { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public double Price { get; set; }
+
+		public Product() {
+			this.ProductID = Guid.NewGuid().ToString();
+		}
+
+		public override string ToString() {
+			return $"ProductID: {this.ProductID} \n" +
+				   $"Name: {this.Name} \n" +
+				   $"Description: {this.Description} \n" +
+				   $"Price: {this.Price:C} \n";
+		}
+	}
+}
---
+++ Example09/Program.cs
@@ -0,0 +1,51 @@
+﻿using Example09.Models;
+using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Example09 {
+	class Program {
+		static void Main(string[] args) {
+			// SERIALIZATION
+			// A technique that can be used to convert virtual objects to files that a computer can easily process (read or write).
+
+			// Creating an object to be serialized.
+			Product product = new Product() { 
+				Name = "Guitar",
+				Description = "A very cool guitar.",
+				Price = 14999.00
+			};
+
+			// Serializing:
+
+			// In this example we will serialize objects as XML.
+			// In order to serialize objects, I need a serializer.
+			XmlSerializer serializer = new XmlSerializer(typeof(Product));
+
+			// Streams are required to serialize.
+			using (Stream stream = new FileStream($"C:\\temp\\{product.ProductID}.xml", FileMode.Create)) {
+				serializer.Serialize(stream, product);
+			}
+
+			Console.WriteLine($"Object {product.ProductID} serialized successfully! \n");
+
+			// =======================================================
+
+			// Deserializing:
+
+			// A serializer is also needed to read XML files from the file system.
+			// We are also going to need a stream to read the file.
+
+			string[] files = Directory.GetFiles("C:\\temp");
+
+			foreach (string file in files) {
+				using (Stream stream = new FileStream(file, FileMode.Open)) {
+					Product loadedProduct = (Product)serializer.Deserialize(stream);
+					Console.WriteLine("Object deserialized successfully!");
+
+					Console.WriteLine(loadedProduct.ToString());
+				}
+			}
+		}
+	}
+}
