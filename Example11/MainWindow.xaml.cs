47bb9fec5b2519487c32157db91ada58ba9f4fc0
Luiz Parente <lp.parente@gmail.com>
2022-03-21 20:20:25 -0400

    Graphical User Interfaces.

---
+++ Example11/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Example11 {
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window {
+		public MainWindow() {
+			InitializeComponent();
+
+			// Event listeners
+			btnSignIn.Click += OnSignIn;
+		}
+
+		// Event Handlers
+		private void OnSignIn(object sender, EventArgs e) {
+			// When the Sign In button is clicked
+			string username = txtUsername.Text;
+
+			if (!string.IsNullOrWhiteSpace(username))
+				MessageBox.Show($"{username} signed in.", "Success!", MessageBoxButton.OK, MessageBoxImage.Information);
+			else
+				MessageBox.Show("Please provide a username.", "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+		}
+	}
+}
